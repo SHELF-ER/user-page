957d634b7a57027d3f753f07b2e9c635bdd5636c
JiheeEom <bonjenny1@gmail.com>
2022-05-19 16:46:47 +0900

    5/19_route작업중

--- src/myinfoRender.js
+++ src/myinfoRender.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Main from './js/Main';
+import Myinfo from './js/Myinfo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Main />
+    <Myinfo />
   </React.StrictMode>
 );
 
