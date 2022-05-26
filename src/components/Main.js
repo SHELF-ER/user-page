765f823ad257068d0905f3443ccec4de649b7c4f
EomJihee <bonjenny1@gmail.com>
2022-05-26 18:44:54 +0900

    5/26-3

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,10 @@ import maincss from '../css/main.module.css';
 import usericon from '../img/usericon.png';
 
 function Main() {
+  document.body.style.backgroundColor = '#7196DB';
   return (
     <div className="Main">
-      <header>
+      <header className={maincss.mainheader}>
         <ul className={maincss.login}>
           <li><Link to={'/borrow'}>로그인</Link></li>
           <li><img src={usericon} alt="usericon"/></li>
@@ -14,7 +15,7 @@ function Main() {
       <section className={maincss.main}>
         <h1>&#128366;SHELF-ER</h1>
         <h3>로그인이 필요합니다. ID카드를 태그해주세요.</h3>
-        <li class="selected"><a href="start.html">대  출</a></li>
+        <li class={maincss.selected}><a href="start.html">대  출</a></li>
         <li><a href="find.html">반  납</a></li>
       </section>
     </div>
