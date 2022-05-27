8b30b4306a36f53fbbf72f5dad2ce0af6031f4af
ChaJunWoo18 <wnsdnxla@naver.com>
2022-05-27 17:55:50 +0900

    5/27-CustomerTest구조화

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,32 @@ import Main from './components/Main';
 import Borrow from './components/Borrow';
 import Client from './components/Client';
 import MyBorrow from './components/MyBorrow';
+import Customertest from './components/Customertest';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '엄기기',
+  'birthday': '010124',
+  'gender': '여자',
+  'job': '대학생'
+}, 
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '엄니니',
+  'birthday': '020224',
+  'gender': '여자',
+  'job': '대학생'
+}, 
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '엄디디',
+  'birthday': '030324',
+  'gender': '여자',
+  'job': '대학생'
+}]
 
 function App() {
   return (
@@ -13,6 +39,21 @@ function App() {
         <Route path="/borrow" element={<Borrow />} />
         <Route path="/client" element={<Client />} />
         <Route path="/myborrow" element={<MyBorrow />} />
+        <Route path="/customertest" element={
+          customers.map(c => {
+            return (
+              <Customertest
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        } />
       </Routes>
     </BrowserRouter>
   );
