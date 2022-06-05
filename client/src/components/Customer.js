7cb0657c34f61f30bbd0ec0329f152ed63ecb931
엄지희 <bonjenny1@gmail.com>
2022-06-05 15:53:19 +0900

    6/5

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { TableRow, TableCell } from '@mui/material';
 
 class Customer extends React.Component {
   render() {
 	  return (
-		  <tr>
-				<td>{this.props.id}</td>
-				<td><img src={this.props.image} alt="img"></img></td>
-				<td>{this.props.name}</td>
-				<td>{this.props.birthday}</td>
-				<td>{this.props.gender}</td>
-				<td>{this.props.job}</td>
-		  </tr>
+		  <TableRow>
+				<TableCell>{this.props.id}</TableCell>
+				<TableCell><img src={this.props.image} alt="img"/></TableCell>
+				<TableCell>{this.props.name}</TableCell>
+				<TableCell>{this.props.birthday}</TableCell>
+				<TableCell>{this.props.gender}</TableCell>
+				<TableCell>{this.props.job}</TableCell>
+		  </TableRow>
 	  )
   }
 }
