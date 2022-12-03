c0c54b45a84971461ad23dae808410fab6269dde
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-12-03 16:07:05 +0900

    12/3 - spring boot 연동 시작

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import MyBorrow from './components/MyBorrow';
 import ManagerMain from './components/ManagerMain';
 import Find from './components/Find';
 
+import AppRouter from './component/route/RouterComponent';
 /*const styles = theme => ({
   root: {
     width: '100%',
@@ -49,66 +50,70 @@ function App() {
 
 
     /*const { classes } = this.props;*/
-    const [hello, setHello] = useState('');
+    // const [hello, setHello] = useState('');
 
-    useEffect(() => {
-      axios.get('/api/hello')
-          .then(response => setHello(response.data))
-          .catch(error => console.log(error))
-    }, []);
+    // useEffect(() => {
+    //   axios.get('/api/hello')
+    //       .then(response => setHello(response.data))
+    //       .catch(error => console.log(error))
+    // }, []);
 
-    document.body.style.backgroundColor = 'white';
+    // document.body.style.backgroundColor = 'white';
     
     return (
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/mform" element={<CreateMemberForm />} />
-            <Route path="/donate" element={<BookDonate />} />
-            <Route path="/mlist" element={<MemberList />} />
-            <Route path="/ebook" element={<Ebook />} />
-            <Route path="/main" element={
-                <div>
-                    <p>{hello}</p>
-                    <Main />
-                </div>
-            } />
-          <Route path="/borrow" element={<Borrow />} />
-          <Route path="/client" element={<Client />} />
-          <Route path="/myborrow" element={<MyBorrow />} />
-          {/*<Route path="/customers" element={*/}
-          {/*  <table>*/}
-          {/*    <thead>*/}
-          {/*      <tr>*/}
-          {/*        <th>번호</th>*/}
-          {/*        <th>이미지</th>*/}
-          {/*        <th>이름</th>*/}
-          {/*        <th>생년월일</th>*/}
-          {/*        <th>성별</th>*/}
-          {/*        <th>직업</th>*/}
-          {/*      </tr>*/}
-          {/*    </thead>*/}
-          {/*    <tbody>*/}
-          {/*    { this.state.customers ? this.state.customers.map(c => {*/}
-          {/*      return (*/}
-          {/*        <Customer*/}
-          {/*          key={c.id}*/}
-          {/*          id={c.id}*/}
-          {/*          image={c.image}*/}
-          {/*          name={c.name}*/}
-          {/*          birthday={c.birthday}*/}
-          {/*          gender={c.gender}*/}
-          {/*          job={c.job}*/}
-          {/*        />*/}
-          {/*      );*/}
-          {/*    }) : <Customer/> }*/}
-          {/*    </tbody>*/}
-          {/*  </table>*/}
-          {/*} />*/}
-          <Route path="/managermain" element={<ManagerMain />} />
-          <Route path="/find" element={<Find />} />
-        </Routes>
-      </BrowserRouter>
+      <div>
+        <AppRouter />
+      </div>
+      
+      // <BrowserRouter>
+      //   <Routes>
+      //       <Route path="/" element={<Home />} />
+      //       <Route path="/mform" element={<CreateMemberForm />} />
+      //       <Route path="/donate" element={<BookDonate />} />
+      //       <Route path="/mlist" element={<MemberList />} />
+      //       <Route path="/ebook" element={<Ebook />} />
+      //       <Route path="/main" element={
+      //           <div>
+      //               <p>{hello}</p>
+      //               <Main />
+      //           </div>
+      //       } />
+      //     <Route path="/borrow" element={<Borrow />} />
+      //     <Route path="/client" element={<Client />} />
+      //     <Route path="/myborrow" element={<MyBorrow />} />
+      //     {/*<Route path="/customers" element={*/}
+      //     {/*  <table>*/}
+      //     {/*    <thead>*/}
+      //     {/*      <tr>*/}
+      //     {/*        <th>번호</th>*/}
+      //     {/*        <th>이미지</th>*/}
+      //     {/*        <th>이름</th>*/}
+      //     {/*        <th>생년월일</th>*/}
+      //     {/*        <th>성별</th>*/}
+      //     {/*        <th>직업</th>*/}
+      //     {/*      </tr>*/}
+      //     {/*    </thead>*/}
+      //     {/*    <tbody>*/}
+      //     {/*    { this.state.customers ? this.state.customers.map(c => {*/}
+      //     {/*      return (*/}
+      //     {/*        <Customer*/}
+      //     {/*          key={c.id}*/}
+      //     {/*          id={c.id}*/}
+      //     {/*          image={c.image}*/}
+      //     {/*          name={c.name}*/}
+      //     {/*          birthday={c.birthday}*/}
+      //     {/*          gender={c.gender}*/}
+      //     {/*          job={c.job}*/}
+      //     {/*        />*/}
+      //     {/*      );*/}
+      //     {/*    }) : <Customer/> }*/}
+      //     {/*    </tbody>*/}
+      //     {/*  </table>*/}
+      //     {/*} />*/}
+      //     <Route path="/managermain" element={<ManagerMain />} />
+      //     <Route path="/find" element={<Find />} />
+      //   </Routes>
+      // </BrowserRouter>
     );
 }
 
